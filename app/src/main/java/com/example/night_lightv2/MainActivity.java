2ad72f9f24ea036bf241b017f31a9595fce992c1
Jessica SH Kim <jesskim2613@gmail.com>
2019-10-15 16:53:02 -0700

    Revert "add myAsyncTask"

--- app/src/main/java/com/example/night_lightv2/MainActivity.java
+++ app/src/main/java/com/example/night_lightv2/MainActivity.java
@@ -4,32 +4,15 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
-import java.util.List;
-
 public class MainActivity extends AppCompatActivity {
-    List<String> coordinatesArr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        String[] url = {"https://seung2613.github.io/street-lighting-poles/street-lighting-poles.json", "get"};
-        new myAsyncTask().execute(url);
     }
-
 
     public void onClickMap(View view){
         startActivity(new Intent(this, MapsActivity.class));
