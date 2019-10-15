22a2f70742a3204436dc88600c2a02f65505659f
A01057802 <henrykang720@Gmail.com>
2019-10-15 16:36:51 -0700

    Added Google Map

--- app/src/main/java/com/example/night_lightv2/MapsActivity.java
+++ app/src/main/java/com/example/night_lightv2/MapsActivity.java
@@ -42,6 +42,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+        LatLng BCIT = new LatLng(-34, 151);
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 }
