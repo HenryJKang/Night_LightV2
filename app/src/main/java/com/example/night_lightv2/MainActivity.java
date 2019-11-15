69230d8da70faa9567f1e855620d2a7e464fa6e9
A01057802 <henrykang720@Gmail.com>
2019-11-14 17:33:01 -0800

    Spinner for loading the jsonObject of the Light pole locations,-Added Toast show up if the user press the map while loading - Added

--- app/src/main/java/com/example/night_lightv2/MainActivity.java
+++ app/src/main/java/com/example/night_lightv2/MainActivity.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 
 public class MainActivity extends AppCompatActivity {
-
+    public  myAsyncTask mat;
+    public Button mapbtn;
+    public static ProgressBar pb;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,14 +35,26 @@ public class MainActivity extends AppCompatActivity {
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
 
+        pb= (ProgressBar)findViewById(R.id.progressBar);
+        mat= new myAsyncTask();
+
+
     }
 
 
+    public void onClickMap(View view)
+    {
+        if(mat.getLoadingStatus()== true){
+            startActivity(new Intent(this, MapsActivity.class));
+        }else{
+            loading();
+        }
 
-    public void onClickMap(View view){
-        startActivity(new Intent(this, MapsActivity.class));
     }
     public void onClickSOS(View view){
         startActivity(new Intent(this, SOS.class));
+    }
+    public void loading(){
+        Toast.makeText(getApplicationContext(), "Still Loading...", Toast.LENGTH_SHORT).show();
     }
 }
